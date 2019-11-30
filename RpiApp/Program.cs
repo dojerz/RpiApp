dddb01147421cf3a2151ad2d8a77a690fd0e2d17
tothp-Home <tothp@ASUS5>
2019-11-30 19:16:18 +0100

    Add project files.

---
+++ RpiApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RpiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //new System.Threading.Timer((e) => WriteToDb(),
+            //    null,
+            //    TimeSpan.Zero,
+            //    TimeSpan.FromSeconds(2))
+            Console.WriteLine("Hopp1");
+            while (true)
+            {
+
+            }
+
+            Timer timer = new Timer(WriteToDb, null, 0, 3000);
+
+            Console.WriteLine("Hopp2");
+
+            Console.ReadLine();
+        }
+        static void WriteToDb(Object o)
+        {
+            Console.WriteLine(DateTime.Now);
+
+        }
+
+        async Task TestToDB()
+        {
+            await Console.WriteLine("Kisfaszom");
+        }
+    }
+}
